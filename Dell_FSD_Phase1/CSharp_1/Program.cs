c022991d7877c7a5f6e765679d044ac30b0a764f
dhruba781341 <dhruba.talukdar5@gmail.com>
2021-03-07 13:00:39 +0530

    First commit Csharp program 1

---
+++ Dell_FSD_Phase1/CSharp_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_1
+{
+    class Program
+    {
+        /*
+        static void Main(string[] args)
+        {
+            Console.Write("1st pogram- Hellow Everyone \n");
+            System.Console.WriteLine("Hellow World");
+            Console.ReadKey();
+
+        }
+        */
+    }
+
+    class InheriClass
+    {
+        void method1()
+        {
+            Console.Write("2nd class- Hellow Everyone \n");
+            System.Console.WriteLine("Hellow World");
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
---
+++ Dell_FSD_Phase1/CSharp_1/Program2.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_1
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            int userid;
+            string userName;
+            float userSalary;
+            double usercontact;
+            bool isfresher;
+
+            // Sample Print 
+            Console.Write("2nd pogram \n");
+            System.Console.WriteLine("Enter Keyword :");
+            Console.ReadLine();
+            Console.WriteLine("\n------------------------------\n" );
+
+            // Type casting
+            int num = 100;
+            Console.WriteLine("num :" + num);
+            double Result = 100 * 0.5F;                // Implicite Type casting 
+            Console.WriteLine("Result :" + Result);
+            Console.WriteLine("\n------------------------------\n");
+
+            int num2 = 50;
+            Console.WriteLine("num2 : " + num2);
+            int Result2 = (int)(num2 * 0.5F);                // Explicite Type casting 
+            Console.WriteLine("Result2 :" + Result2);
+            Console.WriteLine("\n------------------------------\n");
+
+             /* <byte> , <short> and <char> gets converted to int before implementing any calculation */
+
+            // Print User Details 
+            try
+            {
+                Console.Write("userid :");
+                userid = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("userName :");
+                userName = Console.ReadLine();
+
+                Console.Write("userSalary :");
+                userSalary = Convert.ToSingle(Console.ReadLine());
+
+                Console.Write("usercontact :");
+                usercontact = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("isfresher :");
+                isfresher = Convert.ToBoolean(Console.ReadLine());
+
+                Console.Write("\n\n User details \n------------------------\n\n");
+                Console.Write("userid: \n" + userid);
+                Console.Write("userName: \n" + userName);
+                Console.Write("userSalary: \n" + userSalary);
+                Console.Write("usercontact: \n" + usercontact);
+                Console.Write("isfresher: \n" + isfresher);
+
+            }
+            catch(Exception e)
+            {
+                Console.Write("Exception" + e);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
